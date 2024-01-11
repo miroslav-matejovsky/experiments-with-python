e4081351cff774504c41b3307ef22347a3fcb656
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2024-01-11 07:36:06 +0100

    working connection to supabase

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy import text
 
 app = FastAPI()
 load_dotenv()
@@ -16,13 +17,23 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={}
 )
 
+with engine.connect() as connection:
+    # result = connection.execute(text("SELECT 1"))
+    result = connection.execute(text('SELECT info FROM public."Zoja"'))
+    for row in result: 
+        print(row)
+            
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+ 
 @app.get("/")
 async def root():
-    
+
     return {
         "message": "Hello World",
         "SQLAlchemy Version": sqlalchemy.__version__
